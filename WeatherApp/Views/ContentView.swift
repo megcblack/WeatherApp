5fb90e5076b4f88e2adf2871c302107f3ff4ef89
mblack <megcblack@gmail.com>
2023-05-18 09:50:24 -0400

    Welcome View with allow location button

---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by M Black on 5/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var body: some View {
+        VStack {
+            
+            
+            if let location = locationManager.location {
+                Text ("Your coordinates are: \(location.latitude) , \(location.longitude)")
+            } else {
+                if locationManager.isLoading {
+                    ProgressView()
+                }
+            }
+            
+            WelcomeView()
+                .environmentObject(locationManager)
+        }
+        .background(Color(hue: 0.681, saturation: 0.834, brightness: 0.423))
+        .preferredColorScheme(.dark)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
